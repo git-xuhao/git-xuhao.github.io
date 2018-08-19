a2c430aebe15c56269a8e4f103212ac33da5c710
xuhao <504105930@qq.com>
2018-08-19 12:46:27 +0800

    add comments

---
+++ _site/README.md
@@ -0,0 +1,11 @@
+# About this Cool Concise Jekyll Blog Theme 🤘🤘🤘
+
+[![GitHub stars](https://img.shields.io/github/stars/Gaohaoyang/gaohaoyang.github.io.svg)](https://github.com/Gaohaoyang/gaohaoyang.github.io/stargazers)
+[![GitHub forks](https://img.shields.io/github/forks/Gaohaoyang/gaohaoyang.github.io.svg)](https://github.com/Gaohaoyang/gaohaoyang.github.io/network)
+[![GitHub issues](https://img.shields.io/github/issues/Gaohaoyang/gaohaoyang.github.io.svg)](https://github.com/Gaohaoyang/gaohaoyang.github.io/issues)
+[![GitHub release](https://img.shields.io/github/release/Gaohaoyang/gaohaoyang.github.io.svg)](https://github.com/Gaohaoyang/gaohaoyang.github.io/releases)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/Gaohaoyang/gaohaoyang.github.io/master/LICENSE)
+
+With the escalation of jekyll version, but I also want to reconstruct my older blog theme, so I did reconstruction and added some features recently. My new blog theme will still be stored in this repository. I will also use this theme in the future. Now I have done basically, then I will focus on issues that users opend to make theme better.
+
+
---
+++ _site/js/waterfall.js
@@ -0,0 +1,109 @@
+/* jshint asi:true */
+//先等图片都加载完成
+//再执行布局函数
+
+/**
+ * 执行主函数
+ * @param  {[type]} function( [description]
+ * @return {[type]}           [description]
+ */
+(function() {
+
+  /**
+     * 内容JSON
+     */
+  var demoContent = [
+    {
+      demo_link: 'https://xuhaoblog.com/KotlinMvp',
+      img_link: 'https://raw.githubusercontent.com/git-xuhao/KotlinMvp/master/screenshot/kotlin-mvp-1.gif',
+      code_link: 'https://github.com/git-xuhao/KotlinMvp',
+      title: 'KotlinMvp',
+      core_tech: 'kotlin mvp rxjava',
+      description: '基于Kotlin+MVP+Retrofit+RxJava+Glide 等架构实现短视频类小项目，简约风格及详细注释,详情点击<a href ="https://github.com/git-xuhao/KotlinMvp">这里</a>。'
+    }, {
+      demo_link: 'https://github.com/git-xuhao/Android-MVP-Arch',
+      img_link: 'https://github-1253441343.cos.ap-chengdu.myqcloud.com/AndroidMVP.png',
+      code_link: 'https://github.com/git-xuhao/Android-MVP-Arch',
+      title: 'Android MVP 基础框架',
+      core_tech: 'Android MVP Retrofit',
+      description: '一个快速搭建MVP+RxJava2+Retrofit 框架,主要是封装有Http网络请求、日志、缓存、加载等待、toast、页面状态布局管理、权限、RxBus、Glide图片加载等组件，方便快速开发新项目、减少开发成本。'
+    }
+  ];
+
+  contentInit(demoContent) //内容初始化
+  waitImgsLoad() //等待图片加载，并执行布局初始化
+}());
+
+/**
+ * 内容初始化
+ * @return {[type]} [description]
+ */
+function contentInit(content) {
+  // var htmlArr = [];
+  // for (var i = 0; i < content.length; i++) {
+  //     htmlArr.push('<div class="grid-item">')
+  //     htmlArr.push('<a class="a-img" href="'+content[i].demo_link+'">')
+  //     htmlArr.push('<img src="'+content[i].img_link+'">')
+  //     htmlArr.push('</a>')
+  //     htmlArr.push('<h3 class="demo-title">')
+  //     htmlArr.push('<a href="'+content[i].demo_link+'">'+content[i].title+'</a>')
+  //     htmlArr.push('</h3>')
+  //     htmlArr.push('<p>主要技术：'+content[i].core_tech+'</p>')
+  //     htmlArr.push('<p>'+content[i].description)
+  //     htmlArr.push('<a href="'+content[i].code_link+'">源代码 <i class="fa fa-code" aria-hidden="true"></i></a>')
+  //     htmlArr.push('</p>')
+  //     htmlArr.push('</div>')
+  // }
+  // var htmlStr = htmlArr.join('')
+  var htmlStr = ''
+  for (var i = 0; i < content.length; i++) {
+    htmlStr += '<div class="grid-item">' + '   <a class="a-img" href="' + content[i].demo_link + '">' + '       <img src="' + content[i].img_link + '">' + '   </a>' + '   <h3 class="demo-title">' + '       <a href="' + content[i].demo_link + '">' + content[i].title + '</a>' + '   </h3>' + '   <p>主要技术：' + content[i].core_tech + '</p>' + '   <p>' + content[i].description + '       <a href="' + content[i].code_link + '">源代码 <i class="fa fa-code" aria-hidden="true"></i></a>' + '   </p>' + '</div>'
+  }
+  var grid = document.querySelector('.grid')
+  grid.insertAdjacentHTML('afterbegin', htmlStr)
+}
+
+/**
+ * 等待图片加载
+ * @return {[type]} [description]
+ */
+function waitImgsLoad() {
+  var imgs = document.querySelectorAll('.grid img')
+  var totalImgs = imgs.length
+  var count = 0
+  //console.log(imgs)
+  for (var i = 0; i < totalImgs; i++) {
+    if (imgs[i].complete) {
+      //console.log('complete');
+      count++
+    } else {
+      imgs[i].onload = function() {
+        // alert('onload')
+        count++
+        //console.log('onload' + count)
+        if (count == totalImgs) {
+          //console.log('onload---bbbbbbbb')
+          initGrid()
+        }
+      }
+    }
+  }
+  if (count == totalImgs) {
+    //console.log('---bbbbbbbb')
+    initGrid()
+  }
+}
+
+/**
+ * 初始化栅格布局
+ * @return {[type]} [description]
+ */
+function initGrid() {
+  var msnry = new Masonry('.grid', {
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 250,
+    isFitWidth: true,
+    gutter: 20
+  })
+}
